bd2c0b3f601897e6adbdcce6eac78fb87e08c5f4
George <microncore@yahoo.co.uk>
2016-03-22 20:07:25 +0000

    Tree being build right, save codes to file

--- Huffman/CA/Huffman.cpp
+++ Huffman/CA/Huffman.cpp
@@ -1,11 +1,12 @@
 #include "Huffman.h"
 #include <iostream>
+#include <fstream>
 
 /***************************************************************************************
 *    Title: An In-Depth Look At Huffman Encoding
-*    Author: KYA @ dreamincode.net
+*    Author: KYA
 *    Date: 21/03/2016
-*    Code version: n/a
+*    Code version: N/A
 *    Availability: http://www.dreamincode.net/forums/blog/324/entry-3150-an-in-depth-look-at-huffman-encoding/
 ***************************************************************************************/
 
@@ -17,6 +18,7 @@ Huffman::Huffman(std::string textIn)
 	buildTree();
 	createCharToBitMap(root, "");
 	testMap();
+	encodeHuffmanToFile();
 }
 
 void Huffman::createCharFreqMap() {
@@ -53,14 +55,18 @@ void Huffman::populateQueue() {
 // build the huffman tree
 void Huffman::buildTree() {
 
-	while (pQ.size() > 1) {
+	while (pQ.size() != 1) {
 		Node* leftSub = new Node(pQ.top()); // store the top node from the PQ
+	//std::cout<< leftSub->freq; // delete just a test of order
 		pQ.pop(); // pop off the top node from the PQ (the one above)
 		Node* rightSub = new Node(pQ.top()); // the same (binary tree, so has to be done twice)
+	//std::cout << rightSub->freq; // delete just a test
 		pQ.pop();
-		pQ.emplace(new Node(leftSub, rightSub)); // push new node (tree), and add up the frequency (populated in PQ based on frequency)
+		Node* newNode = new Node(leftSub, rightSub);
+	std::cout<<newNode->freq;
+		pQ.emplace(newNode); // push new node (tree), and add up the frequency (populated in PQ based on frequency)
 	}
-	root = pQ.top(); // ref the last node as the root
+	root = pQ.top(); // ref(set) the last node as the root
 	pQ.pop(); // pop off the tree from the priority queue
 
 	std::cout << "- Frequency Tree Built. Frequency Of Root Node: " << root->freq << std::endl;
@@ -69,18 +75,17 @@ void Huffman::buildTree() {
 // pre-order traversal of the tree (reach leaf nodes and store the letter and the bit code in the map)
 void Huffman::createCharToBitMap(Node* nodePtr, std::string letterBits) {
 
-	if (!root) { // if root is NULL (has nothing)
+	if (!nodePtr) { // if root is NULL (has nothing)
 		std::cout << "No Tree";
 	}
 	else {
-		if (nodePtr) { // while not at the leaf node (end)
-			if (nodePtr->letter == NULL) { // if there is no letter present in the node (i.e. branches, only frequency here)
-				createCharToBitMap(nodePtr->left, letterBits + '0'); // at every traversal add a bit (left = 0, right = 1)
-				createCharToBitMap(nodePtr->right, letterBits + '1');
-			}
-			else {
-				charToBitMap.insert(std::pair<char, std::string>(nodePtr->letter, letterBits));
-			}
+		if (nodePtr->letter == NULL) { // if there is no letter present in the node (i.e. branches, only frequency here)
+			std::cout<<nodePtr->freq << " ";
+			createCharToBitMap(nodePtr->left, letterBits + '0'); // at every traversal add a bit (left = 0, right = 1)
+			createCharToBitMap(nodePtr->right, letterBits + '1');
+		}
+		else {
+			charToBitMap.insert(std::pair<char, std::string>(nodePtr->letter, letterBits));
 		}
 	}
 }
@@ -95,3 +100,27 @@ void Huffman::testMap() {
 	}
 }
 //////////////////////////////////////////////////////////////////////////////////////
+
+void Huffman::encodeHuffmanToFile() {
+
+	std::ofstream fileToWrite;
+	fileToWrite.open("CharCodes.txt");
+
+	for (int i = 0; i < inputText.size(); i++) {
+		fileToWrite << charToBitMap.find(inputText[i])->second; // finds the letter, then fetches its second (code)
+	}
+	fileToWrite.close();
+}
+
+void Huffman::decodeHuffmanFromFile() {
+
+	std::string text;
+	std::ifstream inputTextFile("CharCodes.txt");
+
+	if (inputTextFile.is_open()) {
+		while (inputTextFile) {
+			std::getline(inputTextFile, text);
+		}
+	}
+	inputTextFile.close();
+}
--- Huffman/CA/Huffman.h
+++ Huffman/CA/Huffman.h
@@ -19,6 +19,8 @@ private:
 	void populateQueue();
 	void buildTree();
 	void createCharToBitMap(Node* nodePtr, std::string letterBits);
+	void encodeHuffmanToFile();
+	void decodeHuffmanFromFile();
 	void testMap();
 	std::priority_queue<Node, std::vector<Node*>, NodeCompare> pQ;	// http://stackoverflow.com/questions/15646780/priority-queue-of-nodes
 	Node* root = nullptr;											// http://stackoverflow.com/questions/16111337/declaring-a-priority-queue-in-c-with-a-custom-comparator
