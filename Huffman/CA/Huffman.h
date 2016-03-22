742211824b208540ef476f1353130c7002214d40
George <microncore@yahoo.co.uk>
2016-03-22 16:23:42 +0000

    Tree not being build right. Don't know why

--- Huffman/CA/Huffman.h
+++ Huffman/CA/Huffman.h
@@ -20,7 +20,7 @@ private:
 	void buildTree();
 	void createCharToBitMap(Node* nodePtr, std::string letterBits);
 	void testMap();
-	std::priority_queue<Node*, std::vector<Node*>, NodeCompare> pQ;	// http://stackoverflow.com/questions/15646780/priority-queue-of-nodes
+	std::priority_queue<Node, std::vector<Node*>, NodeCompare> pQ;	// http://stackoverflow.com/questions/15646780/priority-queue-of-nodes
 	Node* root = nullptr;											// http://stackoverflow.com/questions/16111337/declaring-a-priority-queue-in-c-with-a-custom-comparator
 public:
 	Huffman(std::string textIn);
