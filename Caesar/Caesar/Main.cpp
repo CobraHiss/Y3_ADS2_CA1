1119c1ea0fa174e485845deb4737b0b74dcb049e
George <microncore@yahoo.co.uk>
2016-03-25 14:32:25 +0000

    Started on Caesar cipher encryption

---
+++ Caesar/Caesar/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "ADS2 CA1 (Caesar Cipher), X00114659, 25/03/2016" << std::endl << std::endl;
+
+	std::string plaintext, ciphertext, decryptedText;
+
+	system("pause");
+	return 0;
+}
