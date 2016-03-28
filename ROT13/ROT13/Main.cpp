77e3dcf15fc7ffab1ea5948f92675672e45dd49f
George <microncore@yahoo.co.uk>
2016-03-28 02:00:59 +0100

    ROT13 cipher complete

--- ROT13/ROT13/Main.cpp
+++ ROT13/ROT13/Main.cpp
@@ -9,6 +9,20 @@
 *    Code version: N/A
 *    Availability: https://en.wikipedia.org/wiki/ROT13
 ***************************************************************************************/
+/***************************************************************************************
+*    Title: ROT13 implementation
+*    Author: Sir_Blake_, Corbin
+*    Date: 26/03/2016
+*    Code version: N/A
+*    Availability: http://codereview.stackexchange.com/questions/14569/rot13-implementation
+***************************************************************************************/
+/***************************************************************************************
+*    Title: [C++] ROT-13 string encoder tutorial (beginner to intermediate)
+*    Author: Noble
+*    Date: 26/03/2016
+*    Code version: N/A
+*    Availability: https://facepunch.com/showthread.php?t=1094693
+***************************************************************************************/
 
 int main() {
 
@@ -31,6 +45,48 @@ int main() {
 		system("pause");
 	}
 	inputFile.close();
+
+	std::cout << "\nCIPHERTEXT ::::::::::::::::::::::::::::::::::::::::::::::\n\n";
+
+	for (unsigned int i = 0; i != text.length(); i++) {
+
+		if (text[i] >= 'a' && text[i] <= 'm') {
+			text[i] = text[i] + 13;
+		}
+		else if (text[i] >= 'n' && text[i] <= 'z') {
+			text[i] = text[i] - 13;
+		}
+		else if (text[i] >= 'A' && text[i] <= 'M') {
+			text[i] = text[i] + 13;
+		}
+		else if (text[i] >= 'N' && text[i] <= 'Z') {
+			text[i] = text[i] - 13;
+		}
+	}
+
+	std::cout << text;
+
+	std::cout << "DECRYPTED ::::::::::::::::::::::::::::::::::::::::::::::: \n\n";
+
+	// ROT13 is an inverse cipher (2x13), so to decrypt it, the exact same algorithm is applied
+
+	for (unsigned int i = 0; i != text.length(); i++) {
+
+		if (text[i] >= 'a' && text[i] <= 'm') {
+			text[i] = text[i] + 13;
+		}
+		else if (text[i] >= 'n' && text[i] <= 'z') {
+			text[i] = text[i] - 13;
+		}
+		else if (text[i] >= 'A' && text[i] <= 'M') {
+			text[i] = text[i] + 13;
+		}
+		else if (text[i] >= 'N' && text[i] <= 'Z') {
+			text[i] = text[i] - 13;
+		}
+	}
+
+	std::cout << text;
 
 	system("pause");
 	return 0;
